12c9ed8f7ade52bf181589a6c2b9648339dd4f6a
sayem <shahanazsharmin66@gmail.com>
2023-09-25 00:50:06 +0600

    login kora jay khali

--- MahnYoh/app/src/main/java/com/example/mahnyoh/MainActivity.kt
+++ MahnYoh/app/src/main/java/com/example/mahnyoh/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.mahnyoh
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
@@ -17,21 +19,58 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mahnyoh.ui.theme.MahnYohTheme
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var auth:FirebaseAuth
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        auth= FirebaseAuth.getInstance()
         var login = findViewById<Button>(R.id.login_button)
         val email = findViewById<EditText>(R.id.eMail)
         val password = findViewById<EditText>(R.id.passwords)
         login.setOnClickListener {
-        val intent = Intent(this,Dashboard::class.java)
-            startActivity(intent)
+
+            val mail=email.text.toString()
+            val pass=password.text.toString()
+
+            if(mail.isEmpty()){
+                Toast.makeText(this,"Please enter your email address", Toast.LENGTH_SHORT).show()
+                email.requestFocus()
+                return@setOnClickListener
+
+            }
+            if(!Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
+                Toast.makeText(this,"Please enter a valid email address", Toast.LENGTH_SHORT).show()
+
+                email.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(pass.isEmpty()) {
+                Toast.makeText(this,"Please enter a password", Toast.LENGTH_SHORT).show()
+
+                password.requestFocus()
+                return@setOnClickListener
+            }
+
+
+
+            auth.signInWithEmailAndPassword(mail,pass).addOnCompleteListener(this) { if (it.isSuccessful) {
+                Toast.makeText(this, "Welcome $email", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Dashboard::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Invalid Login Credentials", Toast.LENGTH_SHORT).show()
+            }
+            }
+            }
+
+            
     }
 
-    }
+    
 }
-
-
